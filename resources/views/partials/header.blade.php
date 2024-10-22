63a74b2e643ca0b50039876fc2ed89acd392d371
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-22 16:58:40 +0200

    added images

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,46 @@
 <header>
-    <h2>Header</h2>
+    <div class="container h-100">
+        <div class="h-100 d-flex justify-content-center align-items-center">
+            <img src="../../../src/dc-logo.png" alt="dc-logo">
+
+            <ul class="d-flex flex-wrap">
+                <li class="fw-bold">
+                    CHARACTERS
+                </li>
+                <li class="fw-bold">
+                    COMICS
+                </li>
+                <li class="fw-bold">
+                    MOVIES
+                </li>
+                <li class="fw-bold">
+                    TV
+                </li>
+                <li class="fw-bold">
+                    GAMES
+                </li>
+                <li class="fw-bold">
+                    COLLECTIBLES
+                </li>
+                <li class="fw-bold">
+                    VIDEOS
+                </li>
+                <li class="fw-bold">
+                    FANS
+                </li>
+                <li class="fw-bold">
+                    NEWS
+                </li>
+                <li class="fw-bold">
+                    SHOP
+                </li>
+            </ul>
+
+            <input type="search" placeholder="Cerca...">
+        </div>
+    </div>
+
+    <section id="jumbotron">
+
+    </section>
 </header>
